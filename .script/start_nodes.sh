5161a3b421858d7ff1c40928ba936356f1c2efeb
LIU Heyang <lhypds@gmail.com>
2023-09-21 00:24:54 +0900

    Small fix

--- .script/start_nodes.sh
+++ .script/start_nodes.sh
@@ -3,14 +3,14 @@
 # Iterate over all sub-directories
 for dir in */; do
     # Check if docker-compose.yml exists in the directory
-    if [ -f "$dir/docker-compose.yml" ]; then
+    if [ -f "$dir/docker-compose.yaml" ]; then
         echo "Starting Docker Compose in $dir"
 
         # Change to the directory
         cd "$dir" || continue
 
         # Start the Docker Compose instance
-        docker-compose up -d
+        docker compose up --build -d
 
         # Go back to the parent directory
         cd ..
